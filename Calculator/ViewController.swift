7751d0ad79f57b2b7a24bf43c3b037d2c695a93c
Davy Hausser <athen@Davys-MBP.home>
2017-02-15 19:46:32 +0000

    completed implementation as per lecture

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Davy Hausser on 09/12/2016.
+//  Copyright © 2016 Davy Hausser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathemathicalSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathemathicalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
+
